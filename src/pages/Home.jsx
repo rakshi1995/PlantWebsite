140df6a7521c8b79dc93fa835870161fae2e5a3c
rakshitak <rakshita.kotian@vistaura.com>
2023-10-23 18:33:19 +0530

    file names changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import Header from "../components/Header";
 import Body from "../components/Body";
 import Categories from "../components/Categories";
-import Featured from "../components/Featured";
-import Blog from "../components/Blog";
+import Featured from "../components/featured";
+import Blog from "../components/blog";
 import Subscribe from "../Subscribe";
 import Footer from "../components/footer";
 

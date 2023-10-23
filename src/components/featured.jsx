0da2cbf552684e7548a83995d31b4d7be3bd7a06
rakshitak <rakshita.kotian@vistaura.com>
2023-10-23 18:42:12 +0530

    product link added

--- src/components/featured.jsx
+++ src/components/featured.jsx
@@ -12,7 +12,7 @@ const Featured = () => {
         <div className="w-full lg:w-2/5 mb-8 lg:mb-0">
           <h2 className="text-5xl font-semibold  mb-4 pl-12">Featured</h2>
           <p className="text-lg mb-4 pl-12">
-            Our plants are 100% organic,<br/>we don't use pesticides or<br/>harmful chemicals.
+            Our plants are 100% organic,<br />we don't use pesticides or<br />harmful chemicals.
             But please don't eat them.
           </p>
           <button className="bg-white text-black font-semibold py-2 px-10 mb-4 ml-12 hover:bg-blue-600 shadow-md border-b-2 border-black">
@@ -21,36 +21,40 @@ const Featured = () => {
         </div>
         <div className="w-full lg:w-3/5 flex justify-between items-center">
           <div className="relative w-1/3">
-          <img
-              src="pictures/plant1.png"
-              alt=" Plant 1"
-              className=" h-80 w-50 object-cover  mb-2 max-w-full"
-            />
+            <Link to="/product">
+              <img
+                src="pictures/plant1.png"
+                alt=" Plant 1"
+                className=" h-80 w-50 object-cover  mb-2 max-w-full"
+              />
+            </Link>
             <div className="absolute bottom-0 left-0 right-0 bg-white p-2 text-sm">
               <p className="text-gray-600">Plant 1</p>
               <p className="text-gray-800">150&euro;</p>
             </div>
           </div>
           <div className="relative w-1/3 pb-8">
-          <img
-              src="pictures/plant2.png"
-              alt="Plant2 1"
-              className=" h-80 w-50 object-cover  mb-2 max-w-full"
-            />
+            <Link to="/product">
+              <img
+                src="pictures/plant2.png"
+                alt="Plant2 1"
+                className=" h-80 w-50 object-cover  mb-2 max-w-full"
+              />
+            </Link>
             <div className="absolute bottom-0 left-0 right-0 bg-white p-2 text-sm">
               <p className="text-gray-600">Plant 2</p>
               <p className="text-gray-800">70&euro;</p>
             </div>
           </div>
           <div className="relative w-1/3">
-          <Link to="/product">
-          <img
-              src="pictures/plant3.png"
-              alt="plant3 "
-              className=" h-80 w-50 object-cover  mb-2 max-w-full"
-            />
-            </Link> 
-          
+            <Link to="/product">
+              <img
+                src="pictures/plant3.png"
+                alt="plant3 "
+                className=" h-80 w-50 object-cover  mb-2 max-w-full"
+              />
+            </Link>
+
             <div className="absolute bottom-0 left-0 right-0 bg-white p-2 text-sm">
               <p className="text-gray-600">Plant 3</p>
               <p className="text-gray-800">50&euro;</p>
